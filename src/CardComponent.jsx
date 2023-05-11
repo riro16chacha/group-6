d61b0040d43b7d0de1b4a7facb6fa935c6d3e924
Riro Maclaurine Chacha <riro.chacha@student.moringaschool.com>
2023-05-11 20:03:12 +0300

    group 6

--- src/CardComponent.jsx
+++ src/CardComponent.jsx
@@ -3,7 +3,13 @@ function Card(){
     return(
         <>
         <h1 className="text-red-300">Product Card</h1>
-        
+        <div>
+            <img src="https://i.pinimg.com/564x/90/e5/0f/90e50f08fa452356d5d98fe37dc10431.jpg" alt="" />
+            <h1>Hot Denim</h1>
+            <p>The best jeans trauser in the market.
+                Get it today at a good price while stocks last.
+            </p>
+        </div>
         </>
     )
 }
